be7da6e6f1a2819d5212f7a970b54a75b421d89e
Joseph H Kennedy <me@jhkennedy.org>
2020-03-19 21:10:59 -0800

    Fix entrypoints tests

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -1,9 +1,9 @@
-def test_proc_insar_isce(script_runner):
+def test_hyp3_ci(script_runner):
     ret = script_runner.run('hyp3_ci', '-h')
     assert ret.success
 
 
-def test_proc_insar_isce(script_runner):
+def test_proc_ci(script_runner):
     ret = script_runner.run('proc_ci', '-h')
     assert ret.success
 
